6cadabd12def16edf01e310ea51a789a4737618b
Kirill <pn79122821224@gmail.com>
2021-03-09 15:56:26 +0500

    refactor(blog-test-utils): removes mongoose dependency and withAuthor option

--- src/utils.ts
+++ src/utils.ts
@@ -21,65 +21,50 @@ interface CreatePostOptions {
   additionalData?: { [key: string]: any };
 }
 
-export const createBlogTestUtils = mongoose => {
-  const createUser = (additionalData: Partial<User> = {}): User => {
-    return {
-      name: faker.name.firstName(),
-      email: faker.internet.email().toLowerCase(),
-      website: faker.internet.url(),
-      location: faker.address.city(),
-      password: faker.internet.password(5),
-      ...additionalData
-    };
-  };
-
-  const createAdmin = (): User => createUser({ role: "admin" });
-
-  const createPost = ({
-    withAuthor = false,
-    additionalData = {}
-  }: CreatePostOptions = {}): Post => {
-    const post: Post = {
-      title: faker.lorem.words(5),
-      description: faker.lorem.sentences(5),
-      body: faker.lorem.sentences(20),
-      ...additionalData
-    };
-
-    if (withAuthor) {
-      post.author = mongoose.Types.ObjectId();
-    }
-
-    return post;
-  };
-
-  const createPosts = (
-    count: number,
-    options: CreatePostOptions = {}
-  ): Post[] => createData("post", count, options);
-
-  const createData = (type, count, options = {}) => {
-    const mapTypeToFactoryFn = {
-      post: createPost,
-      user: createUser
-    };
-
-    const resultData = [];
-    let counter = count;
-
-    while (counter) {
-      resultData.push(mapTypeToFactoryFn[type](options));
-      counter -= 1;
-    }
-
-    return resultData;
-  };
-
+export const createUser = (additionalData: Partial<User> = {}): User => {
   return {
-    createUser,
-    createAdmin,
-    createPost,
-    createPosts,
-    createData
+    name: faker.name.firstName(),
+    email: faker.internet.email().toLowerCase(),
+    website: faker.internet.url(),
+    location: faker.address.city(),
+    password: faker.internet.password(5),
+    ...additionalData
   };
+};
+
+export const createAdmin = (): User => createUser({ role: "admin" });
+
+export const createPost = ({
+  additionalData = {}
+}: CreatePostOptions = {}): Post => {
+  const post: Post = {
+    title: faker.lorem.words(5),
+    description: faker.lorem.sentences(5),
+    body: faker.lorem.sentences(20),
+    ...additionalData
+  };
+
+  return post;
+};
+
+export const createPosts = (
+  count: number,
+  options: CreatePostOptions = {}
+): Post[] => createData("post", count, options);
+
+export const createData = (type, count, options = {}) => {
+  const mapTypeToFactoryFn = {
+    post: createPost,
+    user: createUser
+  };
+
+  const resultData = [];
+  let counter = count;
+
+  while (counter) {
+    resultData.push(mapTypeToFactoryFn[type](options));
+    counter -= 1;
+  }
+
+  return resultData;
 };
